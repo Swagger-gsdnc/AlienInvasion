2561be193dd23f072e52c2024a8a88373561a94b
SwaggerW <1357921463@qq.com>
2018-07-10 10:13:25 +0800

    13.2

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame as pg
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game ():
@@ -16,13 +17,15 @@ def run_game ():
     ship=Ship(ai_settings,screen)
     #创建一个用于存储子弹的编组
     bullets=Group()
-    
+    #
+    alien=Alien(ai_settings,screen)
+
     #开始游戏主循环
     while True:
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings,screen,ship,bullets)
+        gf.update_screen(ai_settings,screen,ship,alien,bullets)
 
                 
 run_game()
